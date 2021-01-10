e3e44f22891fcebcc889dc84efa3f2f764c1975e
algo <samandar149official@gmail.com>
2021-01-10 09:36:06 +0500

    finish

--- components/seo.jsx
+++ components/seo.jsx
@@ -8,6 +8,7 @@ export default function SEO({ title, description, image, keywords }) {
       <title>{title ? `${title} | WebSiteName` : `WebSiteName`}</title>
       <meta name='description' content={description} />
       <meta name='keywords' content={keywords} />
+      <meta name='theme-color' content='#ffb142' />
       <meta property='og:type' content='website' />
       <meta
         property='og:title'
@@ -16,12 +17,6 @@ export default function SEO({ title, description, image, keywords }) {
       />
       <meta property='og:description' content={description} key='ogdesc' />
       <meta property='og:site_name' content='WebSiteName' key='ogsitename' />
-      <meta
-        property='og:image'
-        content={image || 'images/banner_bg1.jpg'}
-        key='ogimage'
-      />
-
       <meta name='twitter:card' content='summary' />
       <meta
         name='twitter:title'
@@ -30,7 +25,6 @@ export default function SEO({ title, description, image, keywords }) {
       <meta name='twitter:description' content={description} />
       <meta name='twitter:site' content='WebSiteName' />
       <meta name='twitter:creator' content='Algo' />
-      <meta name='twitter:image' content={image || 'images/banner_bg1.jpg'} />
       <link rel='icon' href='/favicon.ico' />
     </Head>
   )
